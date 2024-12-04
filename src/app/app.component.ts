75f90e136c3502620264e30d5b5ae450d1871e2e
Sergei <persona.four@yandex.ru>
2024-12-04 19:57:04 +0300

    added name, avatar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from "./Common-UI/profile-card/profile-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
