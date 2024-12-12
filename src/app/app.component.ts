0180878c35c7dda602335446889d2430f1a7c4b1
Sergei <persona.four@yandex.ru>
2024-12-12 20:36:55 +0300

    added api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./Common-UI/profile-card/profile-card.component";
+import { ProfileService } from './data/services/profile.service';
+import { observeOn } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ProfileCardComponent],
+  
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'WEB';
+  profileService :ProfileService = inject(ProfileService)
+  profiles: any = []
+json: any;
+
+  constructor () {
+    this.profileService.getTestAccounts()
+      .subscribe(val)
+      })
+      
+
+  }
+
 }
