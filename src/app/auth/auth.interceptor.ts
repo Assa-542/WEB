473347e39feb326eac44a24ccdbe2b357be7b39d
Sergei <persona.four@yandex.ru>
2024-12-18 12:53:23 +0300

    update inteceptor and README

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,18 +1,56 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpHandler, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
 import { AuthService } from "./auth.service";
 import { inject } from "@angular/core";
+import { catchError, switchMap, throwError } from "rxjs";
 
+let isRefrshing = false
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-    const token = inject(AuthService).token
+    const authService = inject(AuthService)
+    const token = authService.token
 
     if (!token) return next(req)
+    if(isRefrshing) {
+        return refreshAndProceed(authService, req, next)
+    }
+  
+    return next(addToken(req, token))
+    .pipe(
+      catchError(error => {
+        if (error.status === 403){
+            return refreshAndProceed(authService, req, next)
 
-    req = req.clone( {
+        }
+
+        return throwError(error)
+      })
+    )
+}
+
+const refreshAndProceed = (
+    authService: AuthService,
+    req: HttpRequest<any>,
+    next: HttpHandlerFn
+    ) => {
+        if(!isRefrshing) {
+            isRefrshing = true
+            return authService.refreshAuthToken()
+             .pipe(
+              switchMap((res) => {
+                isRefrshing = false
+            return next(addToken(req, res.access_token))
+        })
+      )
+     }
+    
+    return next(addToken(req, authService.token!))
+}
+
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+    return req = req.clone( {
         setHeaders: {
             Authorization: `Bearer ${token}`
         }
     })
-
-    return next(req)
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { TokenResponce } from './auth.interface';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   http = inject(HttpClient)
+  router = inject(Router)
   cookieService = inject(CookieService)
   baseApiUrl = `https://icherniakov.ru/yt-course/auth/`
   
   token: string | null = null
   refreshtoken: string | null = null
+  refreshToken: any;
 
   get isAuth() {
     if (!this.token) {
       this.token = this.cookieService.get('token')
+      this.refreshToken = this.cookieService.get('refreshToken')
     }
     return !!this.token
   }
@@ -29,15 +33,43 @@ export class AuthService {
     
     return this.http.post<TokenResponce>(
       `${this.baseApiUrl}token`,
-      fd
+      fd,
       ).pipe(
-        tap( val => {
-        this.token = val.access_token
-        this.refreshtoken = val.refresh_token
-
-        this.cookieService.set('token', this.token)
-        this.cookieService.set('refreshToken', this.refreshtoken)
-        })
+        tap( val => this.saveTokens(val))
       )
   }
+
+
+refreshAuthToken() {
+  return this.http.post<TokenResponce>(
+    `${this.baseApiUrl}refresh`,
+      {
+      refresh_token: this.refreshToken,
+      }
+  )
+  .pipe(
+      tap( val => this.saveTokens(val)),
+    catchError(err => {
+      this.logout()
+      return throwError(err)
+    })
+  )
+}
+
+logout() {
+  this.cookieService.deleteAll()
+  this.token = null
+  this.refreshToken = null
+  this.router.navigate(['/login'])
+  }
+
+saveTokens(res: TokenResponce) {
+    this.token = res.access_token
+    this.refreshtoken = res.refresh_token
+
+    this.cookieService.set('token', this.token)
+    this.cookieService.set('refreshToken', this.refreshtoken)
+  }
+
+
 }
