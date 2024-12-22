35b5d1e49b4d873c59faf2482e394eb0dfea1e0d
Sergei <persona.four@yandex.ru>
2024-12-22 07:55:25 +0300

    added new path in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { LayoutsComponent } from './Common-UI/layouts/layouts.component';
 import { canActivateAuth } from './auth/aceess.guard';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
     {
     path: '', component: LayoutsComponent, children:[
         {path: '', component: SearchPageComponent},
-        {path: 'profile/:id', component: ProfilePageComponent}
+        {path: 'profile/:id', component: ProfilePageComponent},
+        {path: 'settings', component: SettingsPageComponent},
     ],
     canActivate: [canActivateAuth]
    },
