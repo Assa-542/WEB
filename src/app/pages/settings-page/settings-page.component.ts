dfa63aa79a1789bfae20af0072f755f8e3b4cc49
Sergei <persona.four@yandex.ru>
2024-12-22 09:29:56 +0300

    update project

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { ProfileHeaderComponent } from "../../Common-UI/profile-header/profile-header.component";
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProfileService } from '../../data/services/profile.service';
@@ -14,14 +14,42 @@ import { ProfileService } from '../../data/services/profile.service';
   styleUrl: './settings-page.component.scss'
 })
 export class SettingsPageComponent {
+[x: string]: any;
   fb = inject(FormBuilder)
-  rofileService = inject(ProfileService)
+  profileService = inject(ProfileService)
   
   form = this.fb.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
-    username: ['', Validators.required],
+    username: [{value: '', disabled: true}, Validators.required],
     description: [''],
     stack: ['']
   })
+
+  constructor() {
+    effect(() => {
+      //@ts-ignore
+      this.form.patchValue({
+        ...this.profileService.me(),
+      //@ts-ignore
+        stack: this.mergeStack(this.profileService.me()?.stack)
+      })
+    });
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  onSave() {
+    this.form.markAllAsTouched()
+    this.form.updateValueAndValidity()
+
+    if (this.form.invalid) return
+
+    //@ts-ignore
+    firstValueFrom(this.profileService.patchProfile(this.form.value))
+
+  }
+
 }
